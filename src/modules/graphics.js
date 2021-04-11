3fe07390dca295a5eecc3fdb7f2576f4107c5ba8
kalwalt <github@kalwaltart.it>
2021-04-11 19:18:49 +0200

    drawPoem in modules/graphics now

--- src/modules/graphics.js
+++ src/modules/graphics.js
@@ -19,4 +19,18 @@ gm.prototype.drawRect = function(x, y, width, height) {
   this.drawRectangle(x, y, x + width, y + height);
 }
 
+gm.prototype.drawPoem = function(width, height, book, fontSizeTitle, strokeTitle, fillTitle, fontSizeText, strokeText, fillText) {
+    this.fontSize(fontSizeTitle)
+    this.stroke(strokeTitle, 2)
+    this.fill(fillTitle)
+    this.drawText(width/2, height/2, book.poems[0].title)
+    this.fontSize(fontSizeText)
+    this.stroke(strokeText, 2)
+    this.fill(fillText)
+    for (var i=0; i<3; i++){
+      this.drawText(utils.getRandomIntInclusive(10, width-10), utils.getRandomIntInclusive(10, height-10), book.poems[0].text[i])
+    }
+    return this;
+  }
+
 module.exports = gm
