5a5c022626cb8d493c3da85966c8918302dc6cb9
kalwalt <github@kalwaltart.it>
2021-04-12 23:24:37 +0200

    testing the id

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,5 +1,17 @@
 const loadJ = require('../modules/loadJson')
 
+test("test Json id", done => {
+    loadJ.readWithCallback(__dirname + '/../poems/poems.json', (obj) => {
+        try {
+            expect(obj.poems[0].id).toBe(1)
+            done()
+        }
+        catch (error) {
+            done(error);
+            }
+        })
+    })
+
 test("test Json title", done => {
     loadJ.readWithCallback(__dirname + '/../poems/poems.json', (obj) => {
         try {
