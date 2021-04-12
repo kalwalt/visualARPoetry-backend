d12ee6a67402ff0b618c9e9c8e678eccdae9738d
kalwalt <github@kalwaltart.it>
2021-04-12 14:04:11 +0200

    width and height of the image from gm

--- src/main.js
+++ src/main.js
@@ -15,23 +15,25 @@ var glitchParams = {
 // This function draw some geometries and text (poems), glitch the result and save it incrementally with a date.
 function make(url, book) {
     var width, height;
-    width = 2200;
-    height = 1650;
     gm(dir + url)
-    .blur(8, 4)
-    .addNoisyLines(20, 6, width, height, 880)
-    .drawCircles(10, "red", 2, "#ddffbbbb", width, height, 60)
-    .drawRectangles(4, "yellow", 7, "#ffffffbb", width, height)
-    .toBuffer('JPG', function(err, buff) {
-      if (err) return console.dir(arguments)
-      glitch(glitchParams)
-      .fromBuffer(buff)
-      .toBuffer()
-      .then( function( imageBuffer ) {
-        gm(imageBuffer)
-        .drawPoem(w, h, book, 120, "#efe", "#888", 120, "#efe", "#886")
-      .write(dir + '/visual_poetry_' + utils.getDate() + '.jpg', function (err) {
-        if (!err) console.log('done');
+    .size(function(err, val) {
+      width = val.width;
+      height = val.height;
+      this.blur(8, 4)
+      this.addNoisyLines(20, 6, width, height, 880)
+      this.drawCircles(10, "red", 2, "#ddffbbbb", width, height, 60)
+      this.drawRectangles(4, "yellow", 7, "#ffffffbb", width, height)
+      this.toBuffer('JPG', function(err, buff) {
+        if (err) return console.dir(arguments)
+        glitch(glitchParams)
+        .fromBuffer(buff)
+        .toBuffer()
+        .then( function( imageBuffer ) {
+          gm(imageBuffer)
+          .drawPoem(w, h, book, 120, "#efe", "#888", 120, "#efe", "#886")
+          .write(dir + '/visual_poetry_' + utils.getDate() + '.jpg', function (err) {
+            if (!err) console.log('Image saved');
+        })
       })
     })
   })
@@ -62,10 +64,10 @@ function saveInc(url) {
 console.log(utils.getDate())
 
 reader.readWithCallback(__dirname + '/poems/poems.json',(obj) => {
- make('/fishes.jpg', obj)
+  make('/fishes.jpg', obj)
 })
 
-// testing drawRectangles alone
+// testing drawRectangles and othe graphics stuff. Used only for testing.
 var w, h;
 gm(dir + '/fishes.jpg')
 .size(function(err, val) {
