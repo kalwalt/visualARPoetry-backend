cbe4c3eefbdbcb9ee52f538499dafc68e5befa95
kalwalt <github@kalwaltart.it>
2021-04-12 23:44:01 +0200

    prettifying and cleaning the code

--- src/modules/loadJson.js
+++ src/modules/loadJson.js
@@ -1,27 +1,13 @@
 const fs = require("fs");
-//const fs = require("fs").promises;
-const path = require("path");
 
-const readWithCallback = function(url, callback) {
+const readWithCallback = function (url, callback) {
     let obj;
     fs.readFile(url, 'utf8', function (err, data) {
-      if (err) throw err;
-      obj = JSON.parse(data);
-      callback(obj);
-    });  
+        if (err) throw err;
+        obj = JSON.parse(data);
+        callback(obj);
+    });
 }
-
-/* // to develop...
-async function readWithPromises(filename) {
-    try {
-        const filepath = path.join(process.cwd(), filename);
-        const contents = await fs.readFile(filepath, "utf8");
-        console.log("File Contents:", contents);
-    } catch (error) {
-        console.error(error);
-    }
-}
-*/
 
 module.exports = {
     readWithCallback
--- src/tests/test.js
+++ src/tests/test.js
@@ -5,45 +5,41 @@ test("test Json id", done => {
         try {
             expect(obj.poems[0].id).toBe(1)
             done()
-        }
-        catch (error) {
+        } catch (error) {
             done(error);
-            }
-        })
+        }
     })
+})
 
 test("test Json title", done => {
     loadJ.readWithCallback(__dirname + '/../poems/poems.json', (obj) => {
         try {
             expect(obj.poems[0].title).toBe('A lake with fishes')
             done()
-        }
-        catch (error) {
+        } catch (error) {
             done(error);
-            }
-        })
+        }
     })
+})
 
 test("test Json text poem id 1", done => {
     loadJ.readWithCallback(__dirname + '/../poems/poems.json', (obj) => {
         try {
             expect(obj.poems[0].text[0]).toBe('Leaves, ')
             done()
-        }
-        catch (error) {
+        } catch (error) {
             done(error);
-            }
-        })
+        }
     })
+})
 
 test("test Json text poem id 3", done => {
     loadJ.readWithCallback(__dirname + '/../poems/poems.json', (obj) => {
         try {
             expect(obj.poems[2].text[2]).toBe('remaining on the shore')
             done()
-        }
-        catch (error) {
+        } catch (error) {
             done(error);
-            }
-        })
+        }
     })
+})
