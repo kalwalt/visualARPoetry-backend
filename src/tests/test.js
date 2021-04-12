9eb5ad318e558e09ba22bd7b473d076d3a008528
kalwalt <github@kalwaltart.it>
2021-04-12 23:18:01 +0200

    adding other json tests

--- src/tests/test.js
+++ src/tests/test.js
@@ -8,6 +8,30 @@ test("test Json title", done => {
         }
         catch (error) {
             done(error);
-          }
+            }
+        })
+    })
+
+test("test Json text poem id 1", done => {
+    loadJ.readWithCallback(__dirname + '/../poems/poems.json', (obj) => {
+        try {
+            expect(obj.poems[0].text[0]).toBe('Leaves, ')
+            done()
+        }
+        catch (error) {
+            done(error);
+            }
+        })
+    })
+
+test("test Json text poem id 3", done => {
+    loadJ.readWithCallback(__dirname + '/../poems/poems.json', (obj) => {
+        try {
+            expect(obj.poems[2].text[2]).toBe('remaining on the shore')
+            done()
+        }
+        catch (error) {
+            done(error);
+            }
         })
     })
