0f89bd175eb07b8c66eb45851ec930470afbd1fa
kalwalt <github@kalwaltart.it>
2021-04-13 11:53:33 +0200

    new build-test script

---
+++ src/tests/build-test.js
@@ -0,0 +1,64 @@
+const gm = require('./../modules/graphics')
+, utils = require('./../modules/utils')
+, glitch = require('glitch-canvas')
+, dir = __dirname + '/../imgs'
+, fs = require('fs')
+
+var seed, quality, amount, iterations;
+seed = 55;
+quality = 60;
+amount = 12;
+iterations = 5;
+
+var glitchParams = {
+  seed:       seed,
+	quality:    quality,
+	amount:     amount,
+	iterations: iterations
+}
+
+// This function only apply a blur effect and glitch the image. Then the image is saved with a date. Used only for testing.
+function saveInc(url, glitchParams) {
+  gm(dir + url)
+  .resize(220, 220)
+  .blur(10, 6)
+  .toBuffer('JPG', function(err, buff) {
+    if (err) return console.dir(arguments)
+    glitch(glitchParams)
+    .fromBuffer(buff)
+    .toBuffer()
+    .then( function( imageBuffer ) {
+      fs.writeFile( dir + '/visual_poetry_' + utils.getDate() + '.jpg', imageBuffer, function ( err ) {
+        if ( err ) {
+          throw err;
+        } else {
+          console.log( 'fromBufferToPng complete. File saved to', dir + '/visual_poetry_' + utils.getDate() + '.jpg' );
+        }
+    })
+  })
+})
+}
+
+// testing drawRectangles and the graphics stuff. Used only for testing.
+function simpleTest(url) {
+    var w, h;
+    gm(dir + url)
+    .size(function(err, val) {
+        w = val.width;
+        h = val.height;
+        this.fill('green')
+        this.stroke("#880", 4)
+        this.drawCircleWithRadius(220, 220, 50)
+        this.drawCircles(10, "red", 2, "#ddffbbbb", w, h, 60)
+        this.drawRectangles(10, "red", 2, "#ffffffbb", w, h)
+        this.addNoisyLines(20, 6, w, h, 880)
+        if (!err) console.log('rects ok');
+        this.quality(84)
+        this.write(dir + '/draw_test.jpg', function (err) {
+            if (!err) console.log('done');
+        });
+    })
+}
+
+simpleTest('/fishes.jpg')
+saveInc('/fishes.jpg', glitchParams)
