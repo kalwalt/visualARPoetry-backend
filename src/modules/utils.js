6c972c2c0a056e19f14c0ebbd93ed8a65f09af09
kalwalt <github@kalwaltart.it>
2022-01-30 23:34:44 +0100

    new saveFileArr function

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,6 +1,11 @@
 const dateFormat = require("dateformat")
 , now = new Date();
 
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream('list.txt');
+const pathName = writeStream.path;
+
 const getDate = function() {
     return dateFormat(now, "isoDateTime");
 }
@@ -18,6 +23,29 @@ const convertToInt = function(value, min, max) {
     return Math.floor(value * (max - min + 1)) + min;
 }
 
+const saveFileArr = function (folder) {
+    fs.readdir(folder, (err, files) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log(files);
+        files.forEach(value => writeStream.write(`${value}\n`));
+
+        // the finish event is emitted when all data has been flushed from the stream
+        writeStream.on('finish', () => {
+            console.log(`wrote all the array data to file ${pathName}`);
+        });
+
+        // handle the errors on the write process
+        writeStream.on('error', (err) => {
+            console.error(`There is an error writing the file ${pathName} => ${err}`)
+        });
+
+        // close the stream
+        writeStream.end();
+    })
+}
+
 module.exports = {
-    convertToInt, getDate, getRandomIntInclusive
+    convertToInt, getDate, getRandomIntInclusive, saveFileArr
 }
---
+++ src/tests/read-file-test.js
@@ -0,0 +1,3 @@
+const utils = require('./../modules/utils');
+
+utils.saveFileArr('./src/images');
